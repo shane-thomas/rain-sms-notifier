a1e80ecdfefa0762965a4730d452777f1407f8c1
shane <shaneshaji10@gmail.com>
2023-12-14 21:08:07 +0530

    API test

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
 
 APIKEY = ''
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+
 
 #Al Manqaf, Kuwait
-lat = 29.101179
-lon = 48.131699
 
-response = requests.get("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKEY}&units=metric")
+
+weather_params = {
+    "lat": 29.101179,
+    "lon": 48.131699,
+    "units": "metric",
+    "appid": APIKEY
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 data = response.json()
 print(data)
